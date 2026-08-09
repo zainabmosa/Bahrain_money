427a65576036ddb39e7d8b493a33e1a6032f796f
zainab moosa <57153969+zainabmosa@users.noreply.github.com>
2026-08-09 13:39:49 +0300

    Update titles and icons in B_mon.py

--- B_mon.py
+++ B_mon.py
@@ -151,12 +151,12 @@ def predict_currency(image):
     return (pred_class,confidence,preds)
 
 
-st.markdown('<div class="main-title">🇧🇭 Bahraini Banknote Classification Using Deep Learning</div>',unsafe_allow_html=True)
+st.markdown('<div class="main-title">🇧🇭 Bahraini Banknote Classification</div>',unsafe_allow_html=True)
 
 st.markdown('<div class="subtitle">Upload a photo or use your camera to identify a Bahraini banknote</div>',unsafe_allow_html=True)
 
 
-tab_upload, tab_camera = st.tabs(["📁 Upload Image","📷 Use Camera"])
+tab_upload, tab_camera = st.tabs(["📂 Upload Image","📸 Use Camera"])
 
 image_source = None
 
